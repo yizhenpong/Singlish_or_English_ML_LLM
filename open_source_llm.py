df17fc7047e1888855330932cc4ea298fc35cc17
yizhenpong <yizhenpong@gmail.com>
2023-12-24 18:15:11 +0800

    added mistral model

--- open_source_llm.py
+++ open_source_llm.py
@@ -2,6 +2,7 @@
 instructions:
 - download ollama `https://ollama.ai/download`
     - ollama pull llama2
+    - ollama pull mistral
 - pip install langchain
 """
 
@@ -13,6 +14,7 @@ from langchain.prompts import PromptTemplate
 from langchain.output_parsers import ResponseSchema
 from langchain.output_parsers import StructuredOutputParser #PydanticOutputParser
 # from langchain_core.pydantic_v1 import BaseModel, Field, validator
+import langchain
 
 '''
 Run llama2 and get outputs for this large language model
@@ -24,12 +26,17 @@ Run llama2 and get outputs for this large language model
 
 llm = Ollama(model='llama2',
                 callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+llm2 = Ollama(model='mistral',
+                callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
 
 
 '''building schemas'''
 sentence_schema = ResponseSchema(name="sentence", description="This is the input sentence")
 label_schema = ResponseSchema(name="label", 
-        description="""Label for the input sentence, which must be strictly an integer label of 0 or 1""", type="int")
+        description="""
+        Label for the input sentence, which should be an integer label of 0 or 1.
+        Please do not give comments after 0 or 1 of the form '//' or '#'
+        If you are unable to classify it, provide the reason and classify it under 2""", type="int")
 response_schemas = [sentence_schema, label_schema]
 # explanation_schema = ResponseSchema(name="explanation", 
 #         description="Reasoning for why the sentence was classified as the label")
@@ -42,7 +49,8 @@ format_instructions = output_parser.get_format_instructions()
 # print(format_instructions)
 
 template_string = """ 
-    Given this sentence: '{sentence}', classify if it is Singlish (0) or English (1). \n
+    Given this sentence: '{sentence}', classify it as integer 0 or 1. \n
+        note that: 0 represents Singlish while 1 represent English
         Here are some format instructions that you must follow:
         {format_instructions} 
         Ensure that all strings are enclosed in double quotes,
@@ -55,17 +63,41 @@ prompt = PromptTemplate(
     partial_variables={"format_instructions": format_instructions},
 )
 
-def get_output_dict(sentence):
-    _input = prompt.format_prompt(sentence=sentence)
-    # output = llm(_input.to_json)
-    output = llm(_input.to_string())
-    """ to resolve this error:
-        langchain.schema.output_parser.OutputParserException: Got invalid JSON object.
-        https://stackoverflow.com/questions/77396803/langchain-schema-output-parser-outputparserexception-got-invalid-json-object-e
-    """
-    if output.find("\`\`\`\n\`\`\`") != -1:
-        output = output.replace("\`\`\`\n\`\`\`", "\`\`\`")
-    return output_parser.parse(output) #dict type
+def get_output_dict_llama(sentence):
+    try:
+        _input = prompt.format_prompt(sentence=sentence)
+        # output = llm(_input.to_json)
+        output = llm(_input.to_string())
+        """ to resolve this error:
+            langchain.schema.output_parser.OutputParserException: Got invalid JSON object.
+            https://stackoverflow.com/questions/77396803/langchain-schema-output-parser-outputparserexception-got-invalid-json-object-e
+        """
+        if output.find("\`\`\`\n\`\`\`") != -1:
+            output = output.replace("\`\`\`\n\`\`\`", "\`\`\`")
+        return output_parser.parse(output) #dict type
+    except Exception as e:
+        print(f'An unexpected error occurred: {e}')
+        # Handle other exceptions as needed
+        return {"sentence": sentence, "label": 2}
+
+
+def get_output_dict_mistral(sentence):
+    try:
+        _input = prompt.format_prompt(sentence=sentence)
+        # output = llm(_input.to_json)
+        output = llm2(_input.to_string())
+        """ to resolve this error:
+            langchain.schema.output_parser.OutputParserException: Got invalid JSON object.
+            https://stackoverflow.com/questions/77396803/langchain-schema-output-parser-outputparserexception-got-invalid-json-object-e
+        """
+        if output.find("\`\`\`\n\`\`\`") != -1:
+            output = output.replace("\`\`\`\n\`\`\`", "\`\`\`")
+        return output_parser.parse(output) #dict type
+    except Exception as e:
+        print(f'An unexpected error occurred: {e}')
+        # Handle other exceptions as needed
+        return {"sentence": sentence, "label": 2}
+
 
 # def get_output_dict(sentence):
 #    answer=chain.run(sentence)
